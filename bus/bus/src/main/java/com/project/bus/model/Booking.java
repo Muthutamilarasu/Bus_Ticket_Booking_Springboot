92bc5052c5cc652579395b5333aaf775bf3a8788
Muthutamilarasu <muthutamilarasu1@gmail.com>
2024-02-29 12:49:42 +0530

    backend-project-updated

---
+++ bus/bus/src/main/java/com/project/bus/model/Booking.java
@@ -0,0 +1,28 @@
+package com.project.bus.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Booking {
+    @Id
+    private int customerId;
+    private int busId;
+    private String busName;
+    private int ticketId;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "sp_id")
+    private SignUpModel signUpModel;
+}
